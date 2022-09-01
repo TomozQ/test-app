7f0d7481515ab9c438b5d598440256aa8981b965
furuya <furuya.to@uhmfg.co.jp>
2022-09-02 00:04:13 +0900

    proprs

---
+++ src/AppHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppHeader = ( {title, subject, favoriteColor} ) => {
+  return (
+    <h1>{ title }</h1>
+  )
+}
+
+export default AppHeader
--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import component
+import AppHeader from './AppHeader'
 import AppFooter from './AppFooter'
+import AppContent from './AppContent'
 
 // import style
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 const App = () => {
+  const myProps = {
+    title: 'My Cool App!',
+    subject: 'My subject',
+    favoriteColor: 'red'
+  }
   return (
     <div className='app'>
-      <div>
-        <h1>Hello world</h1>
-      </div>
-      <AppFooter />
+        <AppHeader {...myProps} />
+        <AppContent />
+        <AppFooter />
     </div>
   )
 }
